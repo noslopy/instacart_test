6091544e02ef857183b3e1b90465dc827299a875
noslopy <info@noslopy.dev>
2024-07-05 14:05:25 +0200

    Add User endpoints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def show #fix
+    User.all
+  end
+
+  def create
+    return unless params[:username]
+    return unless params[:avatar]
+    user = User.create(params[:username])
+    user.avatar.attach(params[:avatar])
+    user.save
+  end
+ 
+  private
+    def user_params
+      params.permit(:username, :avatar)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  validates :avatar,
+            content_type: [:png, :jpg, message: 'Uploaded image is neither a JPG nor PNG image'],
+            size: { less_than: 200.kilobytes, message: "asd"}
 end
