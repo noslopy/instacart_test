a3d82cd57a730ed8755ea10354400df6c1ea5cfe
Bence Hunor Noszlopi <noslopy@users.noreply.github.com>
2024-07-10 20:30:13 +0200

    User endpoints (#1)
    * Add User endpoints
    
    * Fix readme based on gemfile
    
    * Add ruby version for rbenv or rvm
    
    * Create required endpoints

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 
 **Environment**  
 
-- Ruby version: 2.7.1
-- Rails version: 6.0.2
+- Ruby version: 3.2.2
+- Rails version: 7.0.0
 - Default Port: 8000
 
 **Commands**
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
+  validates :username, :presence => true
+  validates :avatar, attached: true,
+              content_type: {in: ['jpg', 'png'], message: 'Uploaded image is neither a JPG nor PNG image'},
+              size: { less_than: 200.kilobytes , message: 'File too large. Maximum limit of 200KB exceeded' }
 end
