8399ec032a5b12dac7d1922229cfe744c14c6a38
noslopy <info@noslopy.dev>
2024-07-10 20:02:22 +0200

    Create required endpoints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,32 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show #fix
-    User.all
+  def show
+    users = User.select(:avatar, :id, :username)
+    users = users.map {|user| {avatar: user.avatar.blob.filename, id: user.id, username: user.username}}
+    render json: users, status: :ok
   end
 
   def create
-    return unless params[:username]
-    return unless params[:avatar]
-    user = User.create(params[:username])
-    user.avatar.attach(params[:avatar])
-    user.save
-  end
- 
-  private
-    def user_params
-      params.permit(:username, :avatar)
+    unless params[:user][:avatar].present?
+      render json: { errors: ['Avatar can\'t be blank'] }, status: :unprocessable_entity
+      return
     end
+    unless params[:user][:username].present?
+      render json: { errors: ['Username can\'t be blank'] }, status: :unprocessable_entity
+      return
+    end
+
+    user = User.new
+    user.username = params[:user][:username]
+    user.avatar.attach(params[:user][:avatar])
+    begin
+      user.save!
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { errors: e.record.errors[:avatar] }, status: :unprocessable_entity
+      return
+    end
+
+    render json: { avatar: user.avatar.blob.filename, id: user.id, username: user.username }, status: :created
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  validates :avatar,
-            content_type: [:png, :jpg, message: 'Uploaded image is neither a JPG nor PNG image'],
-            size: { less_than: 200.kilobytes, message: "asd"}
+
+  validates :username, :presence => true
+  validates :avatar, attached: true,
+              content_type: {in: ['jpg', 'png'], message: 'Uploaded image is neither a JPG nor PNG image'},
+              size: { less_than: 200.kilobytes , message: 'File too large. Maximum limit of 200KB exceeded' }
 end
